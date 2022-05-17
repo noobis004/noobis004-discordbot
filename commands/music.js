35a3880d129fada804d69edbdd759bf7a9624f58
noobis004 <noobis004@gmail.com>
2022-05-17 11:17:09 +0200

    Update music.js

--- commands/music.js
+++ commands/music.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { GuildMember, interaction } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, generateDependencyReport } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, generateDependencyReport, AudioPlayerStatus } = require('@discordjs/voice');
 const { QueryType} = require('discord-player');
 const config = require('../config.json');
 const ytdb = require('ytdl-core');
 
-
+var isplaying;
 
 
 module.exports = {
@@ -30,21 +30,6 @@ module.exports = {
                 });
         }
 
-        const songinfo = await ytdb.getInfo(url);
-        const song = {
-            title: songinfo.videoDetails.title,
-            length: songinfo.videoDetails.lengthSeconds,
-        };
-       
-        const stream = ytdb(url, {filter: 'audioonly'});
-        const audioplayer = createAudioPlayer();
-        const resource = createAudioResource(stream);
-
-        const connection = joinVoiceChannel({
-            channelId: interaction.member.voice.channelId,
-            guildId: config.guildId,
-            adapterCreator: interaction.channel.guild.voiceAdapterCreator,
-        });
         if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
             return void interaction.reply({
             content: 'You are not in a voice channel!',
@@ -61,19 +46,41 @@ module.exports = {
             ephemeral: true,
             });
         }
+        
+        const songinfo = await ytdb.getInfo(url);
+        const song = {
+            title: songinfo.videoDetails.title,
+            length: songinfo.videoDetails.lengthSeconds,
+        };
+
+        if (isplaying) {
+            return void interaction.reply({
+                content: 'song already playing',
+                ephemeral: true,
+            })
+        }
+       
+        const stream = ytdb(url, {filter: 'audioonly'});
+        const audioplayer = createAudioPlayer();
+        const resource = createAudioResource(stream);
+
+
+        const connection = joinVoiceChannel({
+            channelId: interaction.member.voice.channelId,
+            guildId: config.guildId,
+            adapterCreator: interaction.channel.guild.voiceAdapterCreator,
+        });
+        
         connection.subscribe(audioplayer);
         audioplayer.play(resource);
+        isplaying = true;
         interaction.reply(`Started playing ${song.title}`);
 
-        const songStandofLength = song.length + 2
-        const songlength = songStandofLength + '_000'
         const subscription = connection.subscribe(audioplayer);
-        console.log(songlength);
 
-        if(subscription) {
-            setTimeout(() => subscription.unsubscribe(), songlength);
-        }
-
+        audioplayer.on(AudioPlayerStatus.Idle, () => {
+            isplaying = false;
+        })
     },
     async stop(interaction, player) {
         const connection = joinVoiceChannel({
@@ -84,5 +91,6 @@ module.exports = {
         
         connection.destroy();
         interaction.reply('Music stopped!');
+        isplaying = false;
     }
 };
